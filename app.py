40b352f42c8cf00aa390f8a1744a7a71190bf719
antishchoolun <39624693+antishchoolun@users.noreply.github.com>
2024-06-17 16:11:49 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask
+# app.py
 
-app = Flask(__name__)
+def main():
+    print("Hello, Heroku!")
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    main()
