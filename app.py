1265ec380a4303115208da4b0fcc864f717be4d2
antishchoolun <39624693+antishchoolun@users.noreply.github.com>
2024-06-17 16:47:11 +0400

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import instaloader
 import subprocess
 import os
 
+import os
+import subprocess
+
 def download_instagram_image(profile_id):
     try:
-        # Dynamically set the base directory where images will be downloaded
-        script_directory = os.path.dirname(os.path.abspath(__file__))
-        base_directory = os.path.join(script_directory, 'downloaded')
+        # Set the base directory where images will be downloaded
+        base_directory = '/app/downloaded'
         
         # Ensure the directory exists
         if not os.path.exists(base_directory):
@@ -19,7 +21,7 @@ def download_instagram_image(profile_id):
         command = [instaloader_path, '--dirname-pattern', base_directory, '--', f'-{profile_id}']
         
         # Run the command
-        result = subprocess.run(command, capture_output=True, text=True, cwd=base_directory)
+        result = subprocess.run(command, capture_output=True, text=True, cwd='/app')
         
         # Check for errors
         if result.returncode == 0:
@@ -28,8 +30,12 @@ def download_instagram_image(profile_id):
             images = [f for f in os.listdir(base_directory) if f.endswith('.jpg')]
             if images:
                 image_path = os.path.join(base_directory, images[0])
-                print(f"Downloaded image path: {image_path}")
-                return image_path
+                if os.path.exists(image_path):  # Check if the image file exists
+                    print(f"Downloaded image path: {image_path}")
+                    return image_path
+                else:
+                    print(f"Image file '{image_path}' does not exist.")
+                    return None
             else:
                 print("No image files found in the folder.")
                 return None
@@ -40,7 +46,14 @@ def download_instagram_image(profile_id):
         print(f"An error occurred: {str(e)}")
         return None
 
-# Example usage
-if __name__ == "__main__":
+@app.route('/')
+def hello():
     profile_id = 'C8Ad5VAJWCQ'  # Replace with your desired profile ID
     downloaded_image_path = download_instagram_image(profile_id)
+    if downloaded_image_path:
+        return f"Image downloaded successfully. Path: {downloaded_image_path}"
+    else:
+        return "Failed to download image."
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
